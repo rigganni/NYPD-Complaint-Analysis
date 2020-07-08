2b5d625a5c8d6cc5e2da61a39ab1bcd3d14d5da9
Nick Riggan <riggan@pm.me>
2020-07-08 18:10:44 -0400

    Initial commit

---
+++ src/nyc_crime_airflow_etl.py
@@ -0,0 +1,54 @@
+from datetime import datetime, timedelta
+import urllib.request
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.hooks.S3_hook import S3Hook
+import logging
+
+DEFAULT_ARGS = {
+    'owner': 'Nick Riggan',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 7, 8),
+    'email': ['riggan@pm.me'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG('nyc_crime_analysis', default_args=DEFAULT_ARGS,
+          schedule_interval="@once")
+
+
+def download_store_nyc_crime(**kwargs):
+    """
+    Download and store NYC crime data
+
+    Parameters:
+    kwargs: Keyword arguments
+
+    Returns:
+    None
+    """
+
+    logging.info("Started download and storage of nyc crime data to S3 compatible storage backend")
+
+    # Download NYC crime data
+    urllib.request.urlretrieve("https://data.cityofnewyork.us/api/views/qgea-i56i/rows.csv?accessType=DOWNLOAD", "/tmp/nyc-crime-new-airflow.csv")
+
+    # Upload generated file to Minio
+    # S3 Hook should called "local_minio" should be created already on Airflow
+    s3 = S3Hook('local_minio')
+    s3.load_file("/tmp/nyc-crime-new-airflow.csv",
+                 key=f"nyc-crime-new-airflow.csv",
+                 bucket_name="nyc-crime")
+
+
+# DAG task to download to S3 compatible storage backend
+t1 = PythonOperator(
+    task_id='download_nyc_crime_store_s3',
+    provide_context=True,
+    python_callable=download_store_nyc_crime,
+    dag=dag
+)
---
+++ src/transform_data.py
@@ -0,0 +1,99 @@
+import configparser
+from datetime import datetime
+import os
+from pyspark.sql import SparkSession, types
+from pyspark import SparkContext, SparkConf, SQLContext
+from pyspark.sql.functions import udf, col, monotonically_increasing_id
+from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
+import subprocess
+import sys
+import pandas as pd
+import numpy as np
+
+config = configparser.ConfigParser()
+config.read('/tmp/dl.cfg')
+
+aws_s3_uri = config.get("AWS", "AWS_S3_URI")
+aws_access_key = config.get("AWS", "AWS_ACCESS_KEY_ID")
+aws_secret_key = config.get("AWS", "AWS_SECRET_ACCESS_KEY")
+
+def create_spark_session():
+    """
+    Create SparkSession object
+
+    Parameters:
+    None
+
+    Returns:
+    spark (pyspark.sql.SparkSession): Spark session
+    """
+
+    # Adapted from https://www.jitsejan.com/setting-up-spark-with-minio-as-object-storage.html
+    conf = (
+        SparkConf()
+        .setAppName("NYC Crime Analysis")
+        .set("spark.hadoop.fs.s3a.endpoint", aws_s3_uri)
+        .set("spark.hadoop.fs.s3a.access.key", aws_access_key)
+        .set("spark.hadoop.fs.s3a.secret.key", aws_secret_key)
+        .set("spark.hadoop.fs.s3a.path.style.access", True)
+        .set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+    )
+    sc = SparkContext(conf=conf).getOrCreate()
+    sqlContext = SQLContext(sc)
+    spark = sqlContext.sparkSession
+    return spark
+
+def get_nyc_crime_record_cnt(spark):
+    """
+    NYC crime analysis
+
+    Parameters:
+    spark (pyspark.sql.SparkSession): Spark session
+
+    Returns:
+    None
+    """
+
+    df = spark.read.csv("s3a://nyc-crime/nyc-crime.csv", inferSchema = True, header = True)
+    df.printSchema()
+
+    df.createOrReplaceTempView("nyc_crime_analysis")
+
+    record_cnt = spark.sql("""
+    SELECT COUNT(1) as cnt
+    FROM nyc_crime_analysis;
+    """)
+
+    print(record_cnt.head())
+
+    cmplnt_cnt_by_fr_dt = spark.sql("""
+    SELECT CMPLNT_FR_DT, 
+           COUNT(1) AS CNT
+    FROM nyc_crime_analysis
+    GROUP BY CMPLNT_FR_DT
+    ORDER BY CNT DESC;
+    """)
+
+    i = 1
+    for index, row in cmplnt_cnt_by_fr_dt.select("*").toPandas().iterrows():
+        if i > 10:
+            break
+        print(row["CMPLNT_FR_DT"] + " " + str(row["CNT"]))
+        i += 1
+
+def main():
+    """
+    Analyze NYC crime
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
+
+    spark = create_spark_session()
+    get_nyc_crime_record_cnt(spark)
+
+if __name__ == "__main__":
+    main()
