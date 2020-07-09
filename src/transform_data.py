b356e1a048ac2e44e01a6bfed3f1690911e9b93f
Nick Riggan <riggan@pm.me>
2020-07-09 18:32:07 -0400

    feat: Create CSV via Spark SQL of temperature data

--- src/transform_data.py
+++ src/transform_data.py
@@ -9,6 +9,9 @@ import subprocess
 import sys
 import pandas as pd
 import numpy as np
+#import DBUtils as dbutils
+import boto3
+
 
 config = configparser.ConfigParser()
 config.read('/tmp/dl.cfg')
@@ -16,6 +19,9 @@ config.read('/tmp/dl.cfg')
 aws_s3_uri = config.get("AWS", "AWS_S3_URI")
 aws_access_key = config.get("AWS", "AWS_ACCESS_KEY_ID")
 aws_secret_key = config.get("AWS", "AWS_SECRET_ACCESS_KEY")
+
+#os.environ["aws_access_key_id"] = aws_access_key
+#os.environ["aws_secret_access_key"] = aws_secret_key
 
 def create_spark_session():
     """
@@ -80,6 +86,34 @@ def get_nypd_complaint_record_cnt(spark):
             break
         print(row["CMPLNT_FR_DT"] + " " + str(row["CNT"]))
         i += 1
+
+    df = spark.read.csv("s3a://nypd-complaint/nyc-weather.csv", inferSchema = True, header = True)
+    df.printSchema()
+
+    df.createOrReplaceTempView("nyc_weather")
+
+    record_cnt = spark.sql("""
+    SELECT COUNT(1) as cnt
+    FROM nyc_weather;
+    """)
+    print(record_cnt.head())
+
+    temperature_analysis_by_date = spark.sql("""
+    SELECT to_date(DATE, 'YYYY-MM-DD') AS date, 
+           MAX(HourlyDryBulbTemperature) AS HighTemp,
+           MIN(HourlyDryBulbTemperature) AS LowTemp 
+    FROM nyc_weather 
+    GROUP BY to_date(DATE, 'YYYY-MM-DD') 
+    ORDER BY date DESC;
+    """)
+
+    temperature_analysis_by_date.toPandas().to_csv("/tmp/temperature_analysis_by_date.csv", header=True)
+    s3 = boto3.resource('s3',
+         endpoint_url=aws_s3_uri,
+         aws_access_key_id=aws_access_key,
+         aws_secret_access_key=aws_secret_key)
+
+    s3.Bucket("nypd-complaint").upload_file("/tmp/temperature_analysis_by_date.csv", "temperature_analysis_by_date.csv")
 
 def main():
     """
