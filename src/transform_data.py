387158963ab401ab5dca8bdeba455fd7ba4d18f5
Nick Riggan <riggan@pm.me>
2020-07-13 13:55:42 -0400

    feat: Add env variable to use local or AWS environment

--- src/transform_data.py
+++ src/transform_data.py
@@ -9,59 +9,74 @@ import subprocess
 import sys
 import pandas as pd
 import numpy as np
-#import DBUtils as dbutils
 import boto3
 
 
-config = configparser.ConfigParser()
-config.read('/tmp/dl.cfg')
-
-aws_s3_uri = config.get("AWS", "AWS_S3_URI")
-aws_access_key = config.get("AWS", "AWS_ACCESS_KEY_ID")
-aws_secret_key = config.get("AWS", "AWS_SECRET_ACCESS_KEY")
-
-#os.environ["aws_access_key_id"] = aws_access_key
-#os.environ["aws_secret_access_key"] = aws_secret_key
-
-def create_spark_session():
+def create_spark_session(env="local"):
     """
     Create SparkSession object
 
     Parameters:
-    None
+    env (string): Environment (i.e. local or aws). Default: local
 
     Returns:
     spark (pyspark.sql.SparkSession): Spark session
     """
 
-    # Adapted from https://www.jitsejan.com/setting-up-spark-with-minio-as-object-storage.html
-    conf = (
-        SparkConf()
-        .setAppName("NYPD complaint Analysis")
-        .set("spark.hadoop.fs.s3a.endpoint", aws_s3_uri)
-        .set("spark.hadoop.fs.s3a.access.key", aws_access_key)
-        .set("spark.hadoop.fs.s3a.secret.key", aws_secret_key)
-        .set("spark.hadoop.fs.s3a.path.style.access", True)
-        .set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-    )
-    sc = SparkContext(conf=conf).getOrCreate()
-    sqlContext = SQLContext(sc)
-    spark = sqlContext.sparkSession
+    config = configparser.ConfigParser()
+    if env == "local":
+        config.read('/tmp/local.cfg')
+        aws_s3_uri = config.get("AWS", "AWS_S3_URI")
+        aws_access_key = config.get("AWS", "AWS_ACCESS_KEY_ID")
+        aws_secret_key = config.get("AWS", "AWS_SECRET_ACCESS_KEY")
+
+    # Set Spark connections
+    if env == "local":
+        # Adapted from https://www.jitsejan.com/setting-up-spark-with-minio-as-object-storage.html
+        conf = (
+            SparkConf()
+            .setAppName("NYPD complaint Analysis")
+            .set("spark.hadoop.fs.s3a.endpoint", aws_s3_uri)
+            .set("spark.hadoop.fs.s3a.access.key", aws_access_key)
+            .set("spark.hadoop.fs.s3a.secret.key", aws_secret_key)
+            .set("spark.hadoop.fs.s3a.path.style.access", True)
+            .set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+        )
+        sc = SparkContext(conf=conf).getOrCreate()
+        sqlContext = SQLContext(sc)
+        spark = sqlContext.sparkSession
+    else:
+        spark = SparkSession \
+            .builder \
+            .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
+            .appName("NYPD Complaint Analysis") \
+            .getOrCreate()
+
     return spark
 
-def get_nypd_complaint_record_cnt(spark):
+def get_nypd_complaint_record_cnt(spark, env="local", aws_access_key="", aws_secret_key=""):
     """
     NYPD complaint analysis
 
     Parameters:
     spark (pyspark.sql.SparkSession): Spark session
+    env (string): Environment (i.e. local or aws). Default: local
+    aws_access_key (string): AWS access key
+    aws_secret_key (string): AWS secret key
 
     Returns:
     None
     """
 
+    config = configparser.ConfigParser()
+    if env == "local":
+        config.read('/tmp/local.cfg')
+        aws_s3_uri = config.get("AWS", "AWS_S3_URI")
+        aws_access_key = config.get("AWS", "AWS_ACCESS_KEY_ID")
+        aws_secret_key = config.get("AWS", "AWS_SECRET_ACCESS_KEY")
+
     df = spark.read.csv("s3a://nypd-complaint/nypd-complaint.csv", inferSchema = True, header = True)
-    df.printSchema()
+#   df.printSchema()
 
     df.createOrReplaceTempView("nypd_complaint_analysis")
 
@@ -70,7 +85,7 @@ def get_nypd_complaint_record_cnt(spark):
     FROM nypd_complaint_analysis;
     """)
 
-    print(record_cnt.head())
+#   print(record_cnt.head())
 
     cmplnt_cnt_by_fr_dt = spark.sql("""
     SELECT CMPLNT_FR_DT, 
@@ -80,15 +95,15 @@ def get_nypd_complaint_record_cnt(spark):
     ORDER BY CNT DESC;
     """)
 
-    i = 1
-    for index, row in cmplnt_cnt_by_fr_dt.select("*").toPandas().iterrows():
-        if i > 10:
-            break
-        print(row["CMPLNT_FR_DT"] + " " + str(row["CNT"]))
-        i += 1
+#   i = 1
+#   for index, row in cmplnt_cnt_by_fr_dt.select("*").toPandas().iterrows():
+#       if i > 10:
+#           break
+#       print(row["CMPLNT_FR_DT"] + " " + str(row["CNT"]))
+#       i += 1
 
     df = spark.read.csv("s3a://nypd-complaint/nyc-weather.csv", inferSchema = True, header = True)
-    df.printSchema()
+#   df.printSchema()
 
     df.createOrReplaceTempView("nyc_weather")
 
@@ -96,7 +111,7 @@ def get_nypd_complaint_record_cnt(spark):
     SELECT COUNT(1) as cnt
     FROM nyc_weather;
     """)
-    print(record_cnt.head())
+#   print(record_cnt.head())
 
     temperature_analysis_by_date = spark.sql("""
     SELECT to_date(DATE, 'YYYY-MM-DD') AS date, 
@@ -108,10 +123,15 @@ def get_nypd_complaint_record_cnt(spark):
     """)
 
     temperature_analysis_by_date.toPandas().to_csv("/tmp/temperature_analysis_by_date.csv", header=True)
-    s3 = boto3.resource('s3',
-         endpoint_url=aws_s3_uri,
-         aws_access_key_id=aws_access_key,
-         aws_secret_access_key=aws_secret_key)
+    if env == "local":
+        s3 = boto3.resource('s3',
+             endpoint_url=aws_s3_uri,
+             aws_access_key_id=aws_access_key,
+             aws_secret_access_key=aws_secret_key)
+    else:
+        s3 = boto3.resource('s3',
+             aws_access_key_id=aws_access_key,
+             aws_secret_access_key=aws_secret_key)
 
     s3.Bucket("nypd-complaint").upload_file("/tmp/temperature_analysis_by_date.csv", "temperature_analysis_by_date.csv")
 
@@ -120,14 +140,19 @@ def main():
     Analyze NYPD complaints
 
     Parameters:
-    None
+    env (string): Environment (i.e. local or aws). Default: local
+    aws_access_key (string): AWS access key
+    aws_secret_key (string): AWS secret key
 
     Returns:
     None
     """
 
-    spark = create_spark_session()
-    get_nypd_complaint_record_cnt(spark)
+    env = sys.argv[1]
+    aws_access_key = sys.argv[2]
+    aws_secret_key = sys.argv[3]
+    spark = create_spark_session(env)
+    get_nypd_complaint_record_cnt(spark, env, aws_access_key, aws_secret_key)
 
 if __name__ == "__main__":
     main()
