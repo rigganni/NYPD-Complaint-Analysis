11dc1ecf26c3aabecab9d5a7c358a1d99c6a6973
Nick Riggan <riggan@pm.me>
2020-07-13 17:24:32 -0400

    feat: Add configparser module to pip requirements

--- src/install-requirements.sh
+++ src/install-requirements.sh
@@ -4,3 +4,4 @@ sudo python3 -m pip install wheel
 sudo python3 -m pip install pyspark
 sudo python3 -m pip install pandas
 sudo python3 -m pip install boto3
+sudo python3 -m pip install configparser
